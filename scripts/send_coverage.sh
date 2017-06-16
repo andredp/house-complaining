299b306ed3cc2d2aab830c85c3b11c13ebb815fc
andredp <andre.patricio.1@sapo.pt>
2017-06-16 04:35:23 +0100

    Change coverage report to codacy

--- scripts/send_coverage.sh
+++ scripts/send_coverage.sh
@@ -1,2 +1,2 @@
 # !/usr/bin/env sh
-npm run coverage && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
+npm run coverage && cat ./coverage/lcov.info | ./node_modules/codacy-coverage/bin/codacy-coverage.js && rm -rf ./coverage
