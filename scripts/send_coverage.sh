c173230db1a3dd3c51d627f40cfc7548369081f0
andredp <andre.patricio.1@sapo.pt>
2017-06-16 04:59:17 +0100

    Fixes usage of cat on the send_coverage script

--- scripts/send_coverage.sh
+++ scripts/send_coverage.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-npm run coverage && cat ./coverage/lcov.info | ./node_modules/codacy-coverage/bin/codacy-coverage.js && rm -rf ./coverage
+npm run coverage && ./node_modules/codacy-coverage/bin/codacy-coverage.js < ./coverage/lcov.info && rm -rf ./coverage
